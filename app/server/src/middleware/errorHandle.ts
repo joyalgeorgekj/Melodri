bb779e787b8001377539a6646f4bd13cef1a61dc
Joyal George K J <joyalgeorgekj@gmail.com>
2026-02-06 00:14:43 +0530

    Error Handle middle ware file created

---
+++ app/server/src/middleware/errorHandle.ts
@@ -0,0 +1,15 @@
+import { ErrorRequestHandler } from "express";
+import { MulterError } from "multer";
+
+export const errorHandle: ErrorRequestHandler = (err, _req, res, _next) => {
+    if (err instanceof MulterError)
+        return res
+            .status(400)
+            .json({ ok: false, code: err.code, message: err.message });
+
+    return res.status(500).json({
+        ok: false,
+        code: "Internal Server Error",
+        message: err.message ?? "Internal Server Error",
+    });
+};
