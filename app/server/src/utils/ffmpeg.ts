c5b8470e2ff32605d08b282af5e25a22d5a57500
Joyal George K J <joyalgeorgekj@gmail.com>
2026-02-10 01:44:03 +0530

    Server: Updated ffmpeg

--- app/server/src/utils/ffmpeg.ts
+++ app/server/src/utils/ffmpeg.ts
@@ -5,49 +5,50 @@ export const ffmpeg = (
     inputPath: string,
     timestamp: string,
 ) => {
-    return new Promise<{output: string, response: string}>((resolve, reject) => {
-        const outputPath = inputPath.replace(inputFileName, "output.wav");
+    return new Promise<{ output: string; response: string }>(
+        (resolve, reject) => {
+            const outputPath = inputPath.replace(inputFileName, "output.wav");
 
-        console.log("FFMPEG:", inputPath, "→", outputPath, "@", timestamp);
+            console.log("FFMPEG:", inputPath, "→", outputPath, "@", timestamp);
 
-        const ff = spawn("ffmpeg", [
-            "-y",
-            "-ss",
-            timestamp,
-            "-t",
-            "30",
-            "-i",
-            inputPath,
-            "-ac",
-            "1",
-            "-ar",
-            "16000",
-            "-f",
-            "wav",
-            outputPath,
-        ]);
+            const ff = spawn("ffmpeg", [
+                "-y",
+                "-i",
+                inputPath,
+                "-ss",
+                timestamp,
+                "-t",
+                "30",
+                "-ac",
+                "1",
+                "-ar",
+                "16000",
+                "-f",
+                "wav",
+                outputPath,
+            ]);
 
-        const timeout = setTimeout(() => {
-            ff.kill("SIGKILL");
-            reject(new Error("FFmpeg timeout"));
-        }, 15000);
+            const timeout = setTimeout(() => {
+                ff.kill("SIGKILL");
+                reject(new Error("ffmpeg timeout"));
+            }, 15000);
 
-        ff.stderr.on("data", (data) => {
-            console.error("FFmpeg:", data.toString());
-        });
+            ff.stderr.on("data", (data) => {
+                console.error("ffmpeg:", data.toString());
+            });
 
-        ff.on("error", (err) => {
-            clearTimeout(timeout);
-            reject(err);
-        });
+            ff.on("error", (err) => {
+                clearTimeout(timeout);
+                reject(err);
+            });
 
-        ff.on("close", (code) => {
-            clearTimeout(timeout);
-            if (code === 0) {
-                resolve(
-                    { output: outputPath, response: "OK" }
-                );
-            } else reject(new Error(`FFmpeg exited with code ${code}`));
-        });
-    });
+            ff.on("close", (code) => {
+                clearTimeout(timeout);
+                if (code === 0) {
+                    resolve({ output: outputPath, response: "OK" });
+                } else if(code === 1) reject(new Error(`Invalid input file content`));
+                else reject(new Error(`ffmpeg exited with code ${code}`));
+            });
+        },
+    );
 };
