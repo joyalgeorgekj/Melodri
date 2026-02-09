c366723aa99450f350e5c26c8b824f875f1780b7
Joyal George K J <joyalgeorgekj@gmail.com>
2026-02-10 01:45:22 +0530

    Client: Created platform constant file

---
+++ app/client/src/utils/platforms.ts
@@ -0,0 +1,5 @@
+export const PLATFORMS = {
+  Youtube: "https://music.youtube.com/search?q=",
+  Spotify: "https://open.spotify.com/search/",
+  Apple: "https://music.apple.com/in/search?term=",
+} as const;
